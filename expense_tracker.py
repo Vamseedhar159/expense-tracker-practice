be3acaf1e18c5f390cbb9cc638e05637530ad3a6
Vamseedhar <Vamseedhark159@gmail.com>
2025-08-28 16:14:12 +0530

    Create expense_tracker.py

---
+++ expense_tracker.py
@@ -0,0 +1,15 @@
+expenses = {}
+
+def add_expense(category, amount):
+    if category not in expenses:
+        expenses[category] = []
+    expenses[category].append(amount)
+
+def remove_expense(category, index):
+    if category in expenses and 0 <= index < len(expenses[category]):
+        expenses[category].pop(index)
+
+def total_expense(category):
+    if category in expenses:
+        return sum(expenses[category])
+    return 0
